643f261eb1635b686ca56db53c04ceff5da8c547
Th0github <tdtsijpkens@gmail.com>
2024-05-23 17:40:11 +0200

    Added Toggle for readme and moved output display to right.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,14 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, input, text, br, button)
+import Html exposing (Html, div, input, text, br, button, h1)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (placeholder, value, class)
 import Json.Encode exposing (encode, int, list, object, string)
 import List.Extra
-
+import Http
+import Markdown exposing (toHtml) 
+import Html.Lazy exposing (lazy)
 
 -- MODEL
 type alias Model =
@@ -18,11 +20,14 @@ type alias Model =
     , agentInput : String
     , propositionInputs : List String -- Proposition inputs for each world
     , relationInputs : List (List Int) -- Relation inputs for each agent 2d list to map agent index to relations
+    , readMeContent : String -- Readme content
+    , showPopup : Bool -- Show the popup
+    , showReadMe : Bool -- Show the Readme content
     }
 
-init : Model
-init =
-    { worlds = []
+init : () -> (Model, Cmd Msg)
+init _ =
+    ({ worlds = []
     , agents = []
     , relations = []
     , jsonOutput = ""
@@ -30,7 +35,10 @@ init =
     , agentInput = ""
     , propositionInputs = []
     , relationInputs = []
-    }
+    , readMeContent = ""
+    , showPopup = False
+    , showReadMe = False
+    }, Cmd.none)
 
 -- UPDATE
 
@@ -43,39 +51,42 @@ type Msg
     | AddProposition Int
     | UpdateRelationInput Int String
     | AddRelation Int
+    | RecieveReadMe (Result Http.Error String)
+    | ToogleReadMe 
+    | FetchReadMe
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         UpdateWorldInput input ->
-            { model | worldInput = input }
+            ({ model | worldInput = input }, Cmd.none)
 
         AddWorld ->
             let
                 world = String.toInt model.worldInput |> Maybe.withDefault 0
                 updatedWorlds = model.worlds ++ [( world, [] )]
             in
-            { model | worlds = updatedWorlds, worldInput = "", propositionInputs = model.propositionInputs ++ [""], jsonOutput = toJson { model | worlds = updatedWorlds }}
+            ({ model | worlds = updatedWorlds, worldInput = "", propositionInputs = model.propositionInputs ++ [""], jsonOutput = toJson { model | worlds = updatedWorlds }}, Cmd.none)
 
         UpdateAgentInput input ->
-            { model | agentInput = input }
+            ({ model | agentInput = input }, Cmd.none)
 
         -- Adds the current agent input to the agents list
         AddAgent ->
-            { model
+            ({ model
             | agents = model.agents ++ [model.agentInput],
             relationInputs = model.relationInputs ++ [[]]
             , relations = model.relations ++ [(model.agentInput, [])]
             , agentInput = ""
             , jsonOutput = toJson { model | agents = model.agents ++ [model.agentInput] }
-            }
+            }, Cmd.none)
 
         -- Updates the current proposition input for the given world index
         UpdatePropositionInput index input ->
             let
                 updatedPropositions = List.Extra.updateAt index (\_ -> input) model.propositionInputs
             in
-            { model | propositionInputs = updatedPropositions }
+            ({ model | propositionInputs = updatedPropositions }, Cmd.none)
 
         -- Adds the current proposition input to the propositions list for the given world index from the propositionInputs list
         AddProposition index ->
@@ -83,7 +94,7 @@ update msg model =
                 proposition = String.toInt (List.Extra.getAt index model.propositionInputs |> Maybe.withDefault "") |> Maybe.withDefault 0
                 updatedWorlds = List.indexedMap (\i (w, ps) -> if i == index then (w, ps ++ [proposition]) else (w, ps)) model.worlds
             in
-            { model | worlds = updatedWorlds, propositionInputs = List.indexedMap (\i p -> if i == index then "" else p) model.propositionInputs, jsonOutput = toJson { model |worlds = updatedWorlds} }
+            ({ model | worlds = updatedWorlds, propositionInputs = List.indexedMap (\i p -> if i == index then "" else p) model.propositionInputs, jsonOutput = toJson { model |worlds = updatedWorlds} }, Cmd.none)
 
         --Updates the current relation input for the given agent index
         UpdateRelationInput index input ->
@@ -95,7 +106,7 @@ update msg model =
                 updatedRelationInputs = List.Extra.updateAt index (\_ -> inputAsList) model.relationInputs
                 _ = Debug.log "Updated relation inputs" updatedRelationInputs
             in
-            { model | relationInputs = updatedRelationInputs }
+            ({ model | relationInputs = updatedRelationInputs }, Cmd.none)
 
 
         -- Adds the current relation input to the relations list for the given agent index from the relationInputs list
@@ -113,12 +124,24 @@ update msg model =
                 updatedRelations = List.Extra.updateAt agentIndex updateRelations model.relations
 
                 in
-                { model
+                ({ model
                 | relations = updatedRelations
                 , relationInputs = List.indexedMap (\i r -> if i == agentIndex then [] else r) model.relationInputs
 
                 , jsonOutput = toJson { model | relations = updatedRelations }
-                }
+                }, Cmd.none)
+        
+        FetchReadMe ->
+            (model, fetchedReadMe)
+        
+        RecieveReadMe (Ok content) ->
+            ({ model | readMeContent = content }, Cmd.none)
+        
+        RecieveReadMe (Err _) ->
+            ({model | readMeContent = "Failed to fetch Readme content"}, Cmd.none)
+
+        ToogleReadMe ->
+            ({model | showReadMe = not model.showReadMe}, Cmd.none)
 
 
 -- VIEW
@@ -126,18 +149,30 @@ update msg model =
 --Need to add this to the index.html file to link the css file
 view : Model -> Html Msg
 view model =
-    div [ class "container"]
-        [
-        div [class "container"]  [text "Kripke Model Creator"],
-        input [class "input",  placeholder "Enter world (integer)", onInput UpdateWorldInput, value model.worldInput ] []
-        , button [class "button", onClick AddWorld ] [ text "Add World" ]
-        , br [] []
-        , div [class "container"] (List.indexedMap (worldInputView model) model.worlds)
-        , input [class "input",  placeholder "Enter agent name", onInput UpdateAgentInput, value model.agentInput ] []
-        , button [class "button", onClick AddAgent ] [ text "Add Agent" ]
-        , br [] []
-        , div [class "container"] (List.indexedMap (agentInputView) model.agents)
-        , div [class "container"] [ text "Current JSON Output:", br [] [], text model.jsonOutput ]
+    div [ class "container-flex"]
+        [ div [ class "left-column"]
+            [ div [class "container"] [text "Kripke Model Creator"],
+            input [class "input",  placeholder "Enter world (integer)", onInput UpdateWorldInput, value model.worldInput ] []
+            , button [class "button", onClick AddWorld ] [ text "Add World" ]
+            , br [] []
+            , div [class "container"] (List.indexedMap (worldInputView model) model.worlds)
+            , input [class "input",  placeholder "Enter agent name", onInput UpdateAgentInput, value model.agentInput ] []
+            , button [class "button", onClick AddAgent ] [ text "Add Agent" ]
+            , br [] []
+            , div [class "container"] (List.indexedMap (agentInputView) model.agents)
+            , button [class "button", onClick ToogleReadMe ] [ text "Toggle README/JSON" ]
+            , button [class "button", onClick FetchReadMe ] [ text "Fetch README" ]
+            ]-- , div [class "container"] [ text "Readme:", br [] [], text model.readMeContent ] -- have html render on writeside
+        , div [ class "right-column" ]
+            [ if model.showReadMe then
+                div []
+                    [ h1 [] [ text "REPORT" ]
+                    , div [] [ lazy (Markdown.toHtml []) model.readMeContent ]
+                    ]
+              else
+                div [class "container"]
+                    [ text "Current JSON Output:", br [] [], text model.jsonOutput ]
+            ]
         ]
 
 worldInputView : Model -> Int -> (Int, List Int) -> Html Msg
@@ -177,12 +212,26 @@ toJson model =
         ]
         |> encode 4
 
+-- Fetch Readme content
+fetchedReadMe : Cmd Msg
+fetchedReadMe =
+    Http.get
+        { url = "https://raw.githubusercontent.com/elm/browser/master/README.md"
+        , expect = Http.expectString RecieveReadMe
+        }
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 -- PROGRAM
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
         , update = update
+        , subscriptions = subscriptions
         , view = view
         }
