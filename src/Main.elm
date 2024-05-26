aec6acb9406c25ecf03f558a9fe24d125aac0c59
CUniversityAccount <c.mol@student.vu.nl>
2024-05-26 21:22:25 +0200

    feat: added post

--- src/Main.elm
+++ src/Main.elm
@@ -1,46 +1,43 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, input, text, br, button, h1)
+import Html exposing (Html, br, button, div, h1, input, text)
+import Html.Attributes exposing (class, placeholder, value)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (placeholder, value, class)
+import Html.Lazy exposing (lazy)
+import Http
+import Json.Decode exposing (decodeValue)
 import Json.Encode exposing (encode, int, list, object, string)
 import List.Extra
-import Http
-import Markdown exposing (toHtml) 
-import Html.Lazy exposing (lazy)
+import Markdown exposing (toHtml)
+import Model exposing (Model, newModelEncoder)
+
+
 
 -- MODEL
-type alias Model =
-    { worlds : List (Int, List Int) -- Each world has a list of propositions
-    , agents : List String
-    , relations : List (String, List (List Int)) -- Each agent has a list of world relations
-    , jsonOutput : String
-    , worldInput : String
-    , agentInput : String
-    , propositionInputs : List String -- Proposition inputs for each world
-    , relationInputs : List (List Int) -- Relation inputs for each agent 2d list to map agent index to relations
-    , readMeContent : String -- Readme content
-    , showPopup : Bool -- Show the popup
-    , showReadMe : Bool -- Show the Readme content
-    }
 
-init : () -> (Model, Cmd Msg)
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-    ({ worlds = []
-    , agents = []
-    , relations = []
-    , jsonOutput = ""
-    , worldInput = ""
-    , agentInput = ""
-    , propositionInputs = []
-    , relationInputs = []
-    , readMeContent = ""
-    , showPopup = False
-    , showReadMe = False
-    }, Cmd.none)
+    ( { worlds = []
+      , agents = []
+      , relations = []
+      , jsonOutput = ""
+      , worldInput = ""
+      , agentInput = ""
+      , propositionInputs = []
+      , relationInputs = []
+      , readMeContent = ""
+      , showPopup = False
+      , showReadMe = False
+      }
+    , Cmd.none
+    )
+
+
 
 -- UPDATE
+
 
 type Msg
     = UpdateWorldInput String
@@ -52,167 +49,269 @@ type Msg
     | UpdateRelationInput Int String
     | AddRelation Int
     | RecieveReadMe (Result Http.Error String)
-    | ToogleReadMe 
+    | ToggleReadMe
     | FetchReadMe
+    | PostKripkeModel
+    | PostedKripkeModel (Result Http.Error String)
+    | GotKripkeModel (Result Http.Error String)
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UpdateWorldInput input ->
-            ({ model | worldInput = input }, Cmd.none)
+            ( { model | worldInput = input }, Cmd.none )
 
         AddWorld ->
             let
-                world = String.toInt model.worldInput |> Maybe.withDefault 0
-                updatedWorlds = model.worlds ++ [( world, [] )]
+                world =
+                    String.toInt model.worldInput |> Maybe.withDefault 0
+
+                updatedWorlds =
+                    model.worlds ++ [ ( world, [] ) ]
             in
-            ({ model | worlds = updatedWorlds, worldInput = "", propositionInputs = model.propositionInputs ++ [""], jsonOutput = toJson { model | worlds = updatedWorlds }}, Cmd.none)
+            ( { model
+                | worlds = updatedWorlds
+                , worldInput = ""
+                , propositionInputs = model.propositionInputs ++ [ "" ]
+                , jsonOutput = toJson { model | worlds = updatedWorlds }
+              }
+            , Cmd.none
+            )
 
         UpdateAgentInput input ->
-            ({ model | agentInput = input }, Cmd.none)
+            ( { model | agentInput = input }, Cmd.none )
 
         -- Adds the current agent input to the agents list
         AddAgent ->
-            ({ model
-            | agents = model.agents ++ [model.agentInput],
-            relationInputs = model.relationInputs ++ [[]]
-            , relations = model.relations ++ [(model.agentInput, [])]
-            , agentInput = ""
-            , jsonOutput = toJson { model | agents = model.agents ++ [model.agentInput] }
-            }, Cmd.none)
+            ( { model
+                | agents = model.agents ++ [ model.agentInput ]
+                , relationInputs = model.relationInputs ++ [ [] ]
+                , relations = model.relations ++ [ ( model.agentInput, [] ) ]
+                , agentInput = ""
+                , jsonOutput = toJson { model | agents = model.agents ++ [ model.agentInput ] }
+              }
+            , Cmd.none
+            )
 
         -- Updates the current proposition input for the given world index
         UpdatePropositionInput index input ->
             let
-                updatedPropositions = List.Extra.updateAt index (\_ -> input) model.propositionInputs
+                updatedPropositions =
+                    List.Extra.updateAt index (\_ -> input) model.propositionInputs
             in
-            ({ model | propositionInputs = updatedPropositions }, Cmd.none)
+            ( { model | propositionInputs = updatedPropositions }, Cmd.none )
 
         -- Adds the current proposition input to the propositions list for the given world index from the propositionInputs list
         AddProposition index ->
             let
-                proposition = String.toInt (List.Extra.getAt index model.propositionInputs |> Maybe.withDefault "") |> Maybe.withDefault 0
-                updatedWorlds = List.indexedMap (\i (w, ps) -> if i == index then (w, ps ++ [proposition]) else (w, ps)) model.worlds
+                proposition =
+                    String.toInt (List.Extra.getAt index model.propositionInputs |> Maybe.withDefault "") |> Maybe.withDefault 0
+
+                updatedWorlds =
+                    List.indexedMap
+                        (\i ( w, ps ) ->
+                            if i == index then
+                                ( w, ps ++ [ proposition ] )
+
+                            else
+                                ( w, ps )
+                        )
+                        model.worlds
             in
-            ({ model | worlds = updatedWorlds, propositionInputs = List.indexedMap (\i p -> if i == index then "" else p) model.propositionInputs, jsonOutput = toJson { model |worlds = updatedWorlds} }, Cmd.none)
+            ( { model
+                | worlds = updatedWorlds
+                , propositionInputs =
+                    List.indexedMap
+                        (\i p ->
+                            if i == index then
+                                ""
+
+                            else
+                                p
+                        )
+                        model.propositionInputs
+                , jsonOutput = toJson { model | worlds = updatedWorlds }
+              }
+            , Cmd.none
+            )
 
         --Updates the current relation input for the given agent index
         UpdateRelationInput index input ->
             let
-                _ = Debug.log "Input index" index
-                _ = Debug.log "Input value" input
-                inputAsList = String.words input |> List.map (\n -> String.toInt n |> Maybe.withDefault 0)
-                _ = Debug.log "Input as list" inputAsList
-                updatedRelationInputs = List.Extra.updateAt index (\_ -> inputAsList) model.relationInputs
-                _ = Debug.log "Updated relation inputs" updatedRelationInputs
-            in
-            ({ model | relationInputs = updatedRelationInputs }, Cmd.none)
+                _ =
+                    Debug.log "Input index" index
 
+                _ =
+                    Debug.log "Input value" input
+
+                inputAsList =
+                    String.words input |> List.map (\n -> String.toInt n |> Maybe.withDefault 0)
+
+                _ =
+                    Debug.log "Input as list" inputAsList
+
+                updatedRelationInputs =
+                    List.Extra.updateAt index (\_ -> inputAsList) model.relationInputs
+
+                _ =
+                    Debug.log "Updated relation inputs" updatedRelationInputs
+            in
+            ( { model | relationInputs = updatedRelationInputs }, Cmd.none )
 
         -- Adds the current relation input to the relations list for the given agent index from the relationInputs list
         AddRelation agentIndex ->
             let
-                maybeCurrentRelations = List.Extra.getAt agentIndex model.relationInputs
-                currentRelations = Maybe.withDefault [] maybeCurrentRelations
+                maybeCurrentRelations =
+                    List.Extra.getAt agentIndex model.relationInputs
 
-                updateRelations (name, existingRelations) = (name, existingRelations ++ [currentRelations])
+                currentRelations =
+                    Maybe.withDefault [] maybeCurrentRelations
 
-                _ = Debug.log "Current relations" currentRelations
-                _ = Debug.log "Updated relations" (updateRelations (List.Extra.getAt agentIndex model.relations |> Maybe.withDefault ("", [])))
+                updateRelations ( name, existingRelations ) =
+                    ( name, existingRelations ++ [ currentRelations ] )
+
+                _ =
+                    Debug.log "Current relations" currentRelations
+
+                _ =
+                    Debug.log "Updated relations" (updateRelations (List.Extra.getAt agentIndex model.relations |> Maybe.withDefault ( "", [] )))
 
                 -- Update the relations for the found agent index
-                updatedRelations = List.Extra.updateAt agentIndex updateRelations model.relations
-
-                in
-                ({ model
+                updatedRelations =
+                    List.Extra.updateAt agentIndex updateRelations model.relations
+            in
+            ( { model
                 | relations = updatedRelations
-                , relationInputs = List.indexedMap (\i r -> if i == agentIndex then [] else r) model.relationInputs
+                , relationInputs =
+                    List.indexedMap
+                        (\i r ->
+                            if i == agentIndex then
+                                []
 
+                            else
+                                r
+                        )
+                        model.relationInputs
                 , jsonOutput = toJson { model | relations = updatedRelations }
-                }, Cmd.none)
-        
-        FetchReadMe ->
-            (model, fetchedReadMe)
-        
-        RecieveReadMe (Ok content) ->
-            ({ model | readMeContent = content }, Cmd.none)
-        
-        RecieveReadMe (Err _) ->
-            ({model | readMeContent = "Failed to fetch Readme content"}, Cmd.none)
+              }
+            , Cmd.none
+            )
 
-        ToogleReadMe ->
-            ({model | showReadMe = not model.showReadMe}, Cmd.none)
+        FetchReadMe ->
+            ( model, fetchedReadMe )
+
+        RecieveReadMe (Ok content) ->
+            ( { model | readMeContent = content }, Cmd.none )
+
+        RecieveReadMe (Err _) ->
+            ( { model | readMeContent = "Failed to fetch Readme content" }, Cmd.none )
+
+        ToggleReadMe ->
+            ( { model | showReadMe = not model.showReadMe }, Cmd.none )
+
+        PostKripkeModel ->
+            Debug.log "Post" ( model, postModel model )
+
+        -- let
+        --     _ =
+        --         Debug.log "Post"
+        -- in
+        -- ( model, postModel model )
+        PostedKripkeModel _ ->
+            ( model, Cmd.none )
+
+        GotKripkeModel _ ->
+            Debug.todo "TODO"
+
 
 
 -- VIEW
 --<link rel="stylesheet" type="text/css" href="styles.css">
 --Need to add this to the index.html file to link the css file
+
+
 view : Model -> Html Msg
 view model =
-    div [ class "container-flex"]
-        [ div [ class "left-column"]
-            [ div [class "container"] [text "Kripke Model Creator"],
-            input [class "input",  placeholder "Enter world (integer)", onInput UpdateWorldInput, value model.worldInput ] []
-            , button [class "button", onClick AddWorld ] [ text "Add World" ]
+    div [ class "container-flex" ]
+        [ div [ class "left-column" ]
+            [ div [ class "container" ] [ text "Kripke Model Creator" ]
+            , input [ class "input", placeholder "Enter world (integer)", onInput UpdateWorldInput, value model.worldInput ] []
+            , button [ class "button", onClick AddWorld ] [ text "Add World" ]
             , br [] []
-            , div [class "container"] (List.indexedMap (worldInputView model) model.worlds)
-            , input [class "input",  placeholder "Enter agent name", onInput UpdateAgentInput, value model.agentInput ] []
-            , button [class "button", onClick AddAgent ] [ text "Add Agent" ]
+            , div [ class "container" ] (List.indexedMap (worldInputView model) model.worlds)
+            , input [ class "input", placeholder "Enter agent name", onInput UpdateAgentInput, value model.agentInput ] []
+            , button [ class "button", onClick AddAgent ] [ text "Add Agent" ]
             , br [] []
-            , div [class "container"] (List.indexedMap (agentInputView) model.agents)
-            , button [class "button", onClick ToogleReadMe ] [ text "Toggle README/JSON" ]
-            , button [class "button", onClick FetchReadMe ] [ text "Fetch README" ]
-            ]-- , div [class "container"] [ text "Readme:", br [] [], text model.readMeContent ] -- have html render on writeside
+            , div [ class "container" ] (List.indexedMap agentInputView model.agents)
+            , button [ class "button", onClick ToggleReadMe ] [ text "Toggle README/JSON" ]
+            , button [ class "button", onClick FetchReadMe ] [ text "Fetch README" ]
+            , button [ class "button", onClick PostKripkeModel ] [ text "Post Model" ]
+            ]
+
+        -- , div [class "container"] [ text "Readme:", br [] [], text model.readMeContent ] -- have html render on writeside
         , div [ class "right-column" ]
             [ if model.showReadMe then
                 div []
                     [ h1 [] [ text "REPORT" ]
                     , div [] [ lazy (Markdown.toHtml []) model.readMeContent ]
                     ]
+
               else
-                div [class "container"]
+                div [ class "container" ]
                     [ text "Current JSON Output:", br [] [], text model.jsonOutput ]
             ]
         ]
 
-worldInputView : Model -> Int -> (Int, List Int) -> Html Msg
-worldInputView model index (world, propositions) =
+
+worldInputView : Model -> Int -> ( Int, List Int ) -> Html Msg
+worldInputView model index ( world, propositions ) =
     div [ class "container" ]
         [ text <| "World " ++ String.fromInt world ++ ": "
-        , input [class "input",  placeholder "Add proposition (integer)", onInput (UpdatePropositionInput index), value (List.Extra.getAt index model.propositionInputs |> Maybe.withDefault "") ] []
-        , button [class "button", onClick (AddProposition index) ] [ text "Add Proposition" ]
+        , input [ class "input", placeholder "Add proposition (integer)", onInput (UpdatePropositionInput index), value (List.Extra.getAt index model.propositionInputs |> Maybe.withDefault "") ] []
+        , button [ class "button", onClick (AddProposition index) ] [ text "Add Proposition" ]
         , text <| " Propositions: " ++ String.join ", " (List.map String.fromInt propositions)
         , br [] []
         ]
+
 
 agentInputView : Int -> String -> Html Msg
 agentInputView index agent =
     div [ class "container" ]
         [ text <| "Agent " ++ agent ++ ": "
-          ,  input
-                [class "input",  placeholder "Add relation (list of worlds)"
-                , onInput (UpdateRelationInput index)
-                -- , value (List.Extra.getAt index model.re)
-                ]
-                []
-        , button [class "button",  onClick (AddRelation index) ] [ text "Add Relation" ]
+        , input
+            [ class "input"
+            , placeholder "Add relation (list of worlds)"
+            , onInput (UpdateRelationInput index)
+
+            -- , value (List.Extra.getAt index model.re)
+            ]
+            []
+        , button [ class "button", onClick (AddRelation index) ] [ text "Add Relation" ]
         , br [] []
         ]
 
 
+
 -- JSON ENCODING
+
 
 toJson : Model -> String
 toJson model =
     object
-        [ ("worlds", list (\(w, _) -> int w) model.worlds) ,
-          ("valuations", list (\(w, ps) -> object [("world", int w), ("propositions", list int ps)]) model.worlds)
+        [ ( "worlds", list (\( w, _ ) -> int w) model.worlds )
+        , ( "valuations", list (\( w, ps ) -> object [ ( "world", int w ), ( "propositions", list int ps ) ]) model.worlds )
+
         -- , ("agents", list string model.agents)
-        , ("relations", list (\(a, rs) -> object [("agentName", string a), ("worldRelations", list (list int) rs)]) model.relations)
+        , ( "relations", list (\( a, rs ) -> object [ ( "agentName", string a ), ( "worldRelations", list (list int) rs ) ]) model.relations )
         ]
         |> encode 4
 
+
+
 -- Fetch Readme content
+
+
 fetchedReadMe : Cmd Msg
 fetchedReadMe =
     Http.get
@@ -220,12 +319,32 @@ fetchedReadMe =
         , expect = Http.expectString RecieveReadMe
         }
 
+
+postModel : Model -> Cmd Msg
+postModel model =
+    Http.request
+        { method = "POST"
+        , headers = []
+        , url = "http://127.0.0.1:3000/model"
+        , body = Http.jsonBody (newModelEncoder model)
+        , expect = Http.expectString PostedKripkeModel
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+
 -- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+
+
 -- PROGRAM
+
 
 main : Program () Model Msg
 main =
--- web-app/src/Lib.hs
+++ web-app/src/Lib.hs
@@ -9,7 +9,21 @@ where
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import DB
 import Model
+import Network.Wai.Middleware.Cors (CorsResourcePolicy (corsMethods, corsRequestHeaders), cors, simpleCors, simpleCorsResourcePolicy, simpleHeaders, simpleMethods)
 import Web.Scotty
+  ( ActionM,
+    delete,
+    get,
+    html,
+    json,
+    jsonData,
+    middleware,
+    notFound,
+    post,
+    put,
+    scotty,
+    text,
+  )
 
 -- import Network.HTTP.Types
 
@@ -32,29 +46,35 @@ muddyStart =
     ]
 
 handleRequest :: IO ()
-handleRequest =
-  scotty 3000 $ do
-    get "/" $ do
-      -- handle GET request on "/" URL
-      text "This was a GET request!" -- send 'text/plain' response
-    delete "/" $ do
-      html "This was a DELETE request!" -- send 'text/html' response
-    post "/" $ do
-      text "This was a POST request!"
-    put "/" $ do
-      text "This was a PUT request!"
+handleRequest = scotty 3000 $ do
+  middleware corsMiddleware
+  get "/" $ do
+    -- handle GET request on "/" URL
+    text "This was a GET request!" -- send 'text/plain' response
+  delete "/" $ do
+    html "This was a DELETE request!" -- send 'text/html' response
+  post "/" $ do
+    text "This was a POST request!"
+  put "/" $ do
+    text "This was a PUT request!"
 
-    -- get model (json)
-    get "/model" $ do
-      json muddyStart -- Call Model constructor and encode the result as JSON
+  -- get model (json)
+  get "/model" $ do
+    json muddyStart -- Call Model constructor and encode the result as JSON
+  post "/model" $ do
+    model <- jsonData :: ActionM Model -- Decode body of the POST request as an Model object
+    liftIO $ save model
+    json model
+  notFound $ do
+    text "there is no such route."
+  where
+    corsMiddleware = cors (const $ Just resourcePolicy)
+      where
+        resourcePolicy =
+          simpleCorsResourcePolicy
+            { corsMethods = "DELETE" : "PUT" : simpleMethods, -- simpleMethods are GET,HEAD,POST
+              corsRequestHeaders = "Content-Type" : simpleHeaders
+            }
 
-    -- post model (json)
-    post "/model" $ do
-      model <- jsonData :: ActionM Model -- Decode body of the POST request as an Model object
-      liftIO $ save model
-      json model
-
-    -- handler for when there is no matched route
-    -- (this should be the last handler because it matches all routes)
-    notFound $ do
-      text "there is no such route."
+-- handler for when there is no matched route
+-- (this should be the last handler because it matches all routes)
